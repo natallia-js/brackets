f91c33fcc34aa080c126aa700d2fe51e45747014
Natallia <55149770+natallia-js@users.noreply.github.com>
2019-09-21 00:35:22 +0300

    feat: implement a check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = {};
+  let stack = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    brackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let currentChar = str[i];
+
+    if ((currentChar in brackets) &&
+       ((brackets[currentChar] != currentChar) || (stack.includes(currentChar) === false))) {
+        stack.push(currentChar);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let lastStackBracket = stack.pop();
+
+      if (currentChar != brackets[lastStackBracket]) {
+        return false;
+      }
+    }
+  }
+
+  if (stack.length > 0) {
+      return false;
+  }
+
+  return true;
 }
